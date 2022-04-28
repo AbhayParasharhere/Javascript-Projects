597324b327398111c4ac8fca3e41105b29db9d96
Abhay <abhayparasharr@gmail.com>
2022-04-28 16:13:06 +0530

    Fixed fundamentals folder 1 not opening by making a copy

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1 - Copy/fundamentals01/starter/script.js
@@ -0,0 +1,5 @@
+let country = "India";
+let continent = "Asia";
+let population = "130";
+
+console.log(country + " is in the continent of " + continent + " with over " + population + " million population");
