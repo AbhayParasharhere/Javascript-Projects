0c168764c0e64fddeb5a97c2c6994f4eb4e5ccb8
Abhay <abhayparasharr@gmail.com>
2022-04-29 11:05:16 +0530

    Fixed checkWinner function from conditional operator to normal if else to fix the logic bug

--- coding challenges/Part 2 Fundamentals 2 coding challenges/#1 Avg function/script.js
+++ coding challenges/Part 2 Fundamentals 2 coding challenges/#1 Avg function/script.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 let dolphinScore1 = Number(prompt("Enter the score for dolphin in round 1"));
 let dolphinScore2 = Number(prompt("Enter the score for dolphin in round 2"));
 let dolphinScore3 = Number(prompt("Enter the score for dolphin in round 3"));
@@ -7,13 +7,17 @@ let koalaScore1 = Number(prompt("Enter the score for koalas in round 1"));
 let koalaScore2 = Number(prompt("Enter the score for koalas in round 2"));
 let koalaScore3 = Number(prompt("Enter the score for koalas in round 3"));
 
-const calcAvg = function (score1, score2, score3) {
-    return (score1 + score2 + score3) / 3;
-}
+// const calcAvg = function (score1, score2, score3) {
+//     return (score1 + score2 + score3) / 3;
+// }
+const calcAvg = (a, b, c) => (a + b + c) / 3;
+
 
 const checkWinner = function (avgDolhins, avgKoalas) {
     let result;
-    avgDolhins >= 2 * avgKoalas ? result = `Dolphins win(${avgDolhins} vs. ${avgKoalas})` : result = `Koalas win(${avgKoalas} vs. ${avgDolhins})`;
+    if (avgDolhins >= 2 * avgKoalas) { result = `Dolphins win(${avgDolhins} vs. ${avgKoalas})` }
+    else if (avgKoalas >= 2 * avgDolhins) { result = `Koalas win(${avgKoalas} vs. ${avgDolhins})` }
+    else { result = `No one won avgKoalas is ${avgKoalas} and avgDolphins is ${avgDolhins}`; }
     console.log(result);
     return result;
 }
