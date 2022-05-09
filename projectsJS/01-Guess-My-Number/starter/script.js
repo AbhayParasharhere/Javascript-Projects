6d4dadbb731a9ebe5c6e5a565900c1fefe51ed20
Abhay <abhayparasharr@gmail.com>
2022-05-09 10:25:23 +0530

    Implemented basic functionality in GuessNumber Project including play again button

--- projectsJS/01-Guess-My-Number/starter/script.js
+++ projectsJS/01-Guess-My-Number/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/** 
 console.log(document.querySelector('.message'));
 
 console.log(`Before accesing the value of INPUT field is
@@ -9,3 +10,65 @@ document.querySelector('.guess').value = 999;
 
 console.log(`After changing the value of INPUT field is
  ${document.querySelector('.guess').value}`);
+ **/
+
+const keyVal = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guessVal = Number(document.querySelector('.guess').value);
+
+  // wrong guess
+  if (!guessVal) {
+    document.querySelector('.message').textContent =
+      '⛔ NO INPUT please try again ';
+  }
+
+  // correct guess
+  else if (guessVal === keyVal) {
+    document.querySelector('.message').textContent =
+      '😁 You have got it right ';
+    document.querySelector('body').style.backgroundColor = '#60b348';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = keyVal;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = String(highScore);
+    }
+  }
+
+  // guess is too high
+  else if (guessVal > keyVal) {
+    if (score > 0) {
+      document.querySelector('.message').textContent =
+        ' 😒 TOO high guess, lower your guess';
+      --score;
+      document.querySelector('.score').textContent = String(score);
+    } else {
+      document.querySelector('.message').textContent = ' 😥 YOU LOSE! Score 0';
+    }
+
+    // guess is too low
+  } else {
+    if (score > 0) {
+      document.querySelector('.message').textContent =
+        '🙄 GUESS too low, guess higher';
+      --score;
+      document.querySelector('.score').textContent = String(score);
+    } else {
+      document.querySelector('.message').textContent = ' 😥 YOU LOSE! Score 0';
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  score = 20;
+  document.querySelector('.score').textContent = String(score);
+  document.querySelector('.message').textContent = 'Start Guessing';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+});
