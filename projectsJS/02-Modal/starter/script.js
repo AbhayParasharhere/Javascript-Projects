0606172f9bb4e1b85747130166cf27cf6b008504
Abhay <abhayparasharr@gmail.com>
2022-05-11 11:04:37 +0530

    Project2 JS modals added additional feature to hide modals when escape key is pressed \n and refactored code

--- projectsJS/02-Modal/starter/script.js
+++ projectsJS/02-Modal/starter/script.js
@@ -5,15 +5,28 @@ const overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.close-modal');
 
 const showModals = document.querySelectorAll('.show-modal');
-
-for (let i = 0; i < showModals.length; i++) {
-  showModals[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
-
-closeModalBtn.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < showModals.length; i++) {
+  showModals[i].addEventListener('click', openModal);
+}
+
+closeModalBtn.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
 });
+
+//KeyboardEvent.
