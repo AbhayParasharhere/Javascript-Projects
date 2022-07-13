3371cdd26277976a7f01ac270cb4ae8b7b083d64
Abhay <abhayparasharr@gmail.com>
2022-07-13 11:24:27 +0530

    Completed challenge 4 part array

--- coding challenges/Part 11 Arrays/ch #4 final array methods/script.js
+++ coding challenges/Part 11 Arrays/ch #4 final array methods/script.js
@@ -1,14 +1,72 @@
 'use strict';
 
-const agesData1 = [5, 2, 4, 1, 15, 8, 3];
-const agesData2 = [16, 6, 10, 5, 6, 1, 4];
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
 
-const calcAverageHumanAge = function (ages) {
-  return ages
-    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
-    .filter(age => age >= 18)
-    .reduce((acc, age, index, arr) => acc + age / arr.length, 0);
-};
+dogs.forEach(
+  dog => (dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28))
+);
+console.log(dogs);
 
-console.log(calcAverageHumanAge(agesData1));
-console.log(calcAverageHumanAge(agesData2));
+const sarahDog = dogs.find(dog => dog.owners.includes('Sarah'));
+
+console.log(sarahDog);
+console.log(
+  sarahDog.curFood > sarahDog.recommendedFood
+    ? "Sarah's dog is eating too much"
+    : "Sarah's dog is eating too little"
+);
+
+const { ownersEatTooMuch, ownersEatTooLittle } = dogs.reduce(
+  (sum, dog) => {
+    if (dog.curFood > dog.recommendedFood)
+      sum.ownersEatTooMuch.push(dog.owners);
+    else if (dog.curFood < dog.recommendedFood)
+      sum.ownersEatTooLittle.push(dog.owners);
+    return sum;
+  },
+  { ownersEatTooMuch: [], ownersEatTooLittle: [] }
+);
+
+console.log(ownersEatTooMuch, ownersEatTooLittle);
+
+console.log(`${ownersEatTooMuch.flat().join(' and ')} dogs eat too much `);
+console.log(`${ownersEatTooLittle.flat().join(' and ')} dogs eat too little `);
+
+console.log(
+  `${
+    dogs.some(dog => dog.recommendedFood === dog.curFood)
+      ? 'some dogs are exactly eating'
+      : 'No dog is eating'
+  } at the recommended food portion'`
+);
+
+console.log(
+  `${
+    dogs.some(
+      dog =>
+        dog.curFood > dog.recommendedFood * 0.9 &&
+        dog.curFood < dog.recommendedFood * 1.1
+    )
+      ? 'some dogs are eating OK'
+      : 'No dog is eating OK'
+  } amounts of food portions'`
+);
+
+const okayFoodDogs = dogs.filter(
+  dog =>
+    dog.curFood > dog.recommendedFood * 0.9 &&
+    dog.curFood < dog.recommendedFood * 1.1
+);
+
+console.log(okayFoodDogs);
+
+const dogsCpy = dogs
+  .slice()
+  .sort((a, b) => a.recommendedFood - b.recommendedFood);
+
+console.log(dogsCpy);
